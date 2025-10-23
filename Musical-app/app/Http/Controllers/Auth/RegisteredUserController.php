e839feea9751626a864d1e25b1f1abdad9c3a428
Sam Nicholl <N00240358@iadt.ie>
2025-10-23 18:46:02 +0100

    Fix redirect route in registration process to musiacls.index from musical.index

--- Musical-app/app/Http/Controllers/Auth/RegisteredUserController.php
+++ Musical-app/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -45,6 +45,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('musical.index', absolute: false));
+        return redirect(route('musicals.index', absolute: false));
     }
 }
